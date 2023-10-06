debd2b5818992b11258b8f0967b099b622b0c963
7002_ <97623785+Sienna7002@users.noreply.github.com>
2023-10-06 21:53:21 +0100

    Add files via upload

---
+++ gl0ckrain.sh
@@ -0,0 +1,261 @@
+#!/bin/bash
+#JESSE WE NEED TO COOK
+#fast
+#slow
+#ADAM IS MY FAVOURITE GINGER
+#Created by Hubert Fitzjiggler -Hubert Fitzjiggler
+#daniel helped in this code :)
+#code club people :)
+#Don't copy this code or we will find and kill you ;)
+#FROM THE CREATORS OF SPECIFIER
+#WE ARE NOT RESPONSIBLE FOR ANY DAMAGES OR BANS DUE TO THE USE OF THIS SCRIPT
+#"if anyone from cyber thingy is r`eading this i <3 you"- Daniel
+case $1 in
+        "--david")
+        $CLEARSCREEN                                                                  
+ echo "▒▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo "▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████    ████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo "▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██  ░░▓▓▒░  ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo "▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█ ░███████▓▒ █▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+ echo "▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█  ███████▓▓ █▓▓▓▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo "▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█ ░▒   ▓   ░ █▓▓▓▓▒▒▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█░▒██▓▓█░▒█▓ █▓▓▓▒▒▓▓▒▓▓▓▓▒▓▓▒▓▓▓▓▓▓███▓▓▓▓███▓▓▓▓▓▓▓▓" 
+ echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██░▓░▓▒░ ░░░░█▓▓▓▒▓▓▓▒▓▓▓▓▒▓▓▓▓▓▓▓▓▓█░██████ ▓▓▓▓▓▓▓▓▓" 
+ echo "▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒▒▓██▓▒▒ ██▓▓▓▒▓▓▓▒▒▓▓▓▓▒▒▒▓▓▓▓▓██   █   ██▓▓▓▓▓▓▓▓" 
+ echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███░░▒▓▓▓░░███▓▓▓▓▒▓▓▒▒▒▓▓▓▒▓▓▓▓█████    ▓███▓▓▓▓▓▓▓██" 
+ echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███▒░▒▓▒░░░ ░░░██████▒▓▓▓▒▒▓▒▒▒▓████ ░    ▒███▓▓▓▓▓▓▓▓▒▒▒"
+ echo "█▓▓▓▓▓▓▓▓▓▓▓███▓░░▒░▒██▓░░░▓▒     ▒███▓▒▒▓▓▓████      ▓████▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo " █▓▓▓▓▓▓▓▓███  ▒░░▓▒▓░░░▒▒░░░▒▓▒▒▒░░ ▒▓▓▓███░        ███▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo " █▓▓▓▓▓▓▓█▓░░░▒█░░▓▒▒▓▒▒▓▒▒▓▒▒▓█▒▓▒█ ░███▒   █   █ ████▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓" 
+ echo " █▓▓▓▓▓██░▒░▓▒░█░▒▓▓███▓░▓█▒█▓▒░░▒▓░▓▓    ▓█████ ░███▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒" 
+ echo "▒▓▓▓▓▓██░▒▓▓░▓░▓ ▒▒    ░▓█▓░░▒░▒ ▒    ░▓░  █▒ ▒ ████▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▓▓▓▓" 
+ echo "▓▓▓▓▓██ ▓▒▓▒▓ ░█░░  ███▒░ ░░░▒ ░░░ ░░░   ░ ░▓▓▓▓██▓▓███▓▓▓▓▓▒▓▓▒▒▒▓▓▓▒▓▓" 
+ echo "▓▓▓▓██ ▓▓▒▓░▓█ ▒▒ ███   █  ░░░    ▒░░░██▓██▒▒▓▓▒▒▓▓▓▓▓▓▓▓▒▒▓▓▒▒▒▒▓▓▒▒▓▓▓" 
+ echo "▓▓▓██ ░▓▒▒▒▒▒▒▓ ░ ████░██ ░░   ██ ░░░  ▓ ░▒ ▓▓▒▒▒▒▒▓▓▒▒▓▒▒▒▒▒▒▒▓▓▒▒▒▓▓▓▓" 
+ echo "▓▓▓▓░░▓▓▒▓▒▓░░█▓▓▓░ ▒██▒    ▒██  ░░ ░░░ ░▒░░█▒▒▒▒▒▒▒▒▒▓▒▒▒▓▓▓▓▓▒▒▓▓▓▓▓▓▓" 
+ echo "▓▓▓▒  ▒░░▒░▒▒░░░ █▓░   ▒▓ ███     ░░░░  ▒▒░▓▓▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓███▓▓▓▓▓▓" 
+ echo "▓▓▓████████▒   ██▓       ███████▓▓▒▒░░███░ ▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓" 
+ echo "▒▓▓▓░▓▒░    █▒     ▒▓█████████▒█░░░░ ░░░░░█▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▒▓▓▓▓▓▓▓" 
+ echo "▓▓▓▓▒▓▒▓███▒▒▓█████████████▒  ░░ ░▓▒░▒ ░▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▒▒▓▓▓▓▓▓▓▓▓▓"                          
+                echo "Your were banned at my request. Usually somebody posting photos of me holding a gun is not here for serious discussions of computers."
+                ;;
+        *)
+$CLEARSCREEN
+sudo echo "Sudo OK"
+echo 
+echo "  ▄████  ██▓     ▒█████   ▄████▄   ██ ▄█▀ ██▀███   ▄▄▄       ██▓ ███▄    █ ";
+echo " ██▒ ▀█▒▓██▒    ▒██▒ ███▒▒██▀ ▀█   ██▄█▒ ▓██ ▒ ██▒▒████▄    ▓██▒ ██ ▀█   █ ";
+echo "▒██░▄▄▄░▒██░    ▒██░█ ██▒▒▓█    ▄ ▓███▄░ ▓██ ░▄█ ▒▒██  ▀█▄  ▒██▒▓██  ▀█ ██▒";
+echo "░▓█  ██▓▒██░    ▒███  ██░▒▓▓▄ ▄██▒▓██ █▄ ▒██▀▀█▄  ░██▄▄▄▄██ ░██░▓██▒  ▐▌██▒";
+echo "░▒▓███▀▒░██████▒░ ████▓▒░▒ ▓███▀ ░▒██▒ █▄░██▓ ▒██▒ ▓█   ▓██▒░██░▒██░   ▓██░";
+echo " ░▒   ▒ ░ ▒░▓  ░░ ▒░▒░▒░ ░ ░▒ ▒  ░▒ ▒▒ ▓▒░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░▓  ░ ▒░   ▒ ▒ ";
+echo "  ░   ░ ░ ░ ▒  ░  ░ ▒ ▒░   ░  ▒   ░ ░▒ ▒░  ░▒ ░ ▒░  ▒   ▒▒ ░ ▒ ░░ ░░   ░ ▒░";
+echo "░ ░   ░   ░ ░   ░ ░ ░ ▒  ░        ░ ░░ ░   ░░   ░   ░   ▒    ▒ ░   ░   ░ ░ ";
+echo "      ░     ░  ░    ░ ░  ░ ░      ░  ░      ░           ░  ░ ░           ░ ";
+echo "                         ░                                                 ";
+echo "";                                  
+echo "(FROM THE CREATORS OF SPECIFIER)"
+ENDCOLOR="\e[0m"
+SUCCESS=$(tput setaf 2)
+RED=$(tput setaf 1)
+BOLD=$(tput smso)
+ENDBOLD=$(tput rmso)
+CLEARSCREEN=$(tput clear)
+echo "gl0ckrain v1.0.4 (G0V055)"
+echo "(c) 2023 Sudo group"
+echo "by Gabriel, Ty, Daniel"
+echo "DO NOT DISTRUBUTE - BETA"
+echo "WE ARE NOT RESPONSIBLE FOR ANY DAMAGES OR BANS DUE TO THE USE OF THIS SCRIPT"
+sleep 5
+PS3="Select the run option: "
+select lng in Slow Fast Quit
+do
+    case $lng in
+        "Slow")
+          echo "gl0ckrain $lng "
+          echo "by Gabriel, Ty, Daniel (c) 2023"
+          sleep 3
+          firefox_config () {
+    echo 'pref("general.config.filename", "mozilla.cfg");' | sudo tee -a /usr/lib/firefox/defaults/pref/local-settings.js > /dev/null
+
+    local config='lockPref("browser.safebrowsing.downloads.enabled", true);\n
+    lockPref("dom.disable_open_during_load", true);\n
+    lockPref("xpinstall.whitelist.required", true);\n
+    lockPref("xpinstall.signatures.required", true);\n
+    lockPref("app.update.enabled", true);\n
+    lockPref("app.update.auto", true);\n
+    lockPref("privacy.donottrackheader.enabled", true);\n
+    lockPref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", true);\n
+    lockPref("browser.safebrowsing.downloads.remote.block_uncommon", true);\n
+    lockPref("browser.safebrowsing.malware.enabled", true);\n
+    lockPref("browser.safebrowsing.phishing.enabled", true);\n'
+    echo   $config | sed "s/^ //g" | sudo tee -a /usr/lib/firefox/mozilla.cfg > /dev/null
+}
+        sleep 4m 29s
+          remove_malware () {
+          echo "${RED}Please make sure you are 100% sure that there is no critcial services in this before running!!${ENDCOLOR}"
+           declare -a arr=(john, abc, sqlmap, aria2
+                    aquisition, bitcomet, bitlet, bitspirit
+                    endless-sky, zenmap, minetest, minetest-server
+                    armitage, crack, apt pureg knocker, aircrack-ng
+                    airbase-ng, hydra, freeciv
+                    wireshark, tshark
+                    hydra-gtk, netcat, netcat-traditional, netcat-openbsd
+                    netcat-ubuntu, netcat-minimal, qbittorrent, ctorrent
+                    ktorrent, rtorrent, deluge, transmission-common
+                    transmission-bittorrent-client, tixati, frostwise, vuse
+                    irssi, transmission-gtk, utorrent, kismet
+                    medusa, telnet, exim4, telnetd
+                    bind9, crunch, tcpdump, tomcat
+                    tomcat6, vncserver, tightvnc, tightvnc-common
+                    tightvncserver, vnc4server, nmdb, dhclient
+                    telnet-server, ophcrack, cryptcat, cups
+                    cupsd, tcpspray, ettercap
+                    wesnoth, snort, pryit
+                    weplab, wireshark, nikto, lcrack
+                    postfix, snmp, icmp, dovecot
+                    pop3, p0f, dsniff, hunt
+                    ember, nbtscan, rsync, freeciv-client xtras
+                    freeciv-data, freeciv-server, freeciv-client-gtk
+                    )
+
+    for i in "${arr[@]}"
+    do
+        sudo $APT purge -y --force-yes $i
+    done
+}
+          sudo apt upgrade
+          echo   "${SUCCESS}Upgraded software."
+          sudo apt update
+          sleep 1m 23s
+          echo   "${SUCCESS}Updated software."
+          sudo cp /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades.BACKUP-$(date '+%Y-%m-%d')
+          sleep 3m 41s
+          echo   "${SUCCESS}Daily updates enabled."    
+          sudo systemctl restart ssh
+          sleep 4m 21s
+          echo   "${SUCCESS}SSH restarted."
+          sudo find /etc/apt -type f -name '*.list'  xec sed -i 's/^#\(deb.*-backports.*\)/\1/; s/^#\(deb.*-updates.*\)/\1/; s/^#\(deb.*-proposed.*\)/\1/; s/^#\(deb.*-security.*\)/\1/' {} +
+          sleep 1m 12s
+          sudo ufw enable
+          sleep 36s
+          echo   "${SUCCESS}UFW enabled."
+                    echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⠆⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⡿⠁⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⠟⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⡿⠃⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀"
+echo "⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠺⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠈⠻⣿⣿⣦⣄⠀⠀⠀⠀⣠⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⡀⢠⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣼⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+          echo -e "${ENDCOLOR}success";;
+        "Fast")
+        
+          echo "gl0ckrain $lng "
+          echo "by Gabriel, Ty, Daniel (c) 2023"
+          sleep 3
+          firefox_config () {
+    echo 'pref("general.config.filename", "mozilla.cfg");' | sudo tee -a /usr/lib/firefox/defaults/pref/local-settings.js > /dev/null
+
+    local config='lockPref("browser.safebrowsing.downloads.enabled", true);\n
+    lockPref("dom.disable_open_during_load", true);\n
+    lockPref("xpinstall.whitelist.required", true);\n
+    lockPref("xpinstall.signatures.required", true);\n
+    lockPref("app.update.enabled", true);\n
+    lockPref("app.update.auto", true);\n
+    lockPref("privacy.donottrackheader.enabled", true);\n
+    lockPref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", true);\n
+    lockPref("browser.safebrowsing.downloads.remote.block_uncommon", true);\n
+    lockPref("browser.safebrowsing.malware.enabled", true);\n
+    lockPref("browser.safebrowsing.phishing.enabled", true);\n'
+    echo   $config | sed "s/^ //g" | sudo tee -a /usr/lib/firefox/mozilla.cfg > /dev/null
+}
+          remove_malware () {
+
+    echo "${RED}Please make sure you are 100% sure that there is no critcial services in this before running!!${ENDCOLOR}"
+    declare -a arr=(john, abc, sqlmap, aria2
+                    aquisition, bitcomet, bitlet, bitspirit
+                    endless-sky, zenmap, minetest, minetest-server
+                    armitage, crack, apt pureg knocker, aircrack-ng
+                    airbase-ng, hydra, freeciv
+                    wireshark, tshark
+                    hydra-gtk, netcat, netcat-traditional, netcat-openbsd
+                    netcat-ubuntu, netcat-minimal, qbittorrent, ctorrent
+                    ktorrent, rtorrent, deluge, transmission-common
+                    transmission-bittorrent-client, tixati, frostwise, vuse
+                    irssi, transmission-gtk, utorrent, kismet
+                    medusa, telnet, exim4, telnetd
+                    bind9, crunch, tcpdump, tomcat
+                    tomcat6, vncserver, tightvnc, tightvnc-common
+                    tightvncserver, vnc4server, nmdb, dhclient
+                    telnet-server, ophcrack, cryptcat, cups
+                    cupsd, tcpspray, ettercap
+                    wesnoth, snort, pryit
+                    weplab, wireshark, nikto, lcrack
+                    postfix, snmp, icmp, dovecot
+                    pop3, p0f, dsniff, hunt
+                    ember, nbtscan, rsync, freeciv-client xtras
+                    freeciv-data, freeciv-server, freeciv-client-gtk
+                    )
+
+    for i in "${arr[@]}"
+    do
+        sudo $APT purge -y --force-yes $i
+    done
+}
+          sudo apt-get upgrade
+          echo   "${SUCCESS}Upgraded software."
+          anti_malware_software () {
+    # Files necessary:
+    #   NONE
+    sudo $APT install -y chkrootkit clamav rkhunter apparmor apparmor-profiles 
+}
+          sudo apt update
+          echo   "${SUCCESS}Updated software."
+          sudo cp /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades.BACKUP-$(date '+%Y-%m-%d')
+          echo "Daily updates enabled."    
+          sudo systemctl restart ssh
+          echo   "${SUCCESS}SSH restarted."
+          sudo find /etc/apt -type f -name '*.list'  xec sed -i 's/^#\(deb.*-backports.*\)/\1/; s/^#\(deb.*-updates.*\)/\1/; s/^#\(deb.*-proposed.*\)/\1/; s/^#\(deb.*-security.*\)/\1/' {} +
+          sudo ufw enable
+          echo   "${SUCCESS}UFW enabled."
+                    echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⠆⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⡿⠁⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⠟⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⡿⠃⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀"
+echo "⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠺⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠈⠻⣿⣿⣦⣄⠀⠀⠀⠀⣠⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⡀⢠⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣼⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+          echo -e "${ENDCOLOR}success";;
+          
+        "Quit")
+echo "  ▄████  ▒█████   ▒█████  ▓█████▄  ▄▄▄▄ ▓██   ██▓▓█████ ";
+echo " ██▒ ▀█▒▒██▒  ██▒▒██▒  ██▒▒██▀ ██▌▓█████▄▒██  ██▒▓█   ▀ ";
+echo "▒██░▄▄▄░▒██░  ██▒▒██░  ██▒░██   █▌▒██▒ ▄██▒██ ██░▒███   ";
+echo "░▓█  ██▓▒██   ██░▒██   ██░░▓█▄   ▌▒██░█▀  ░ ▐██▓░▒▓█  ▄ ";
+echo "░▒▓███▀▒░ ████▓▒░░ ████▓▒░░▒████▓ ░▓█  ▀█▓░ ██▒▓░░▒████▒";
+echo " ░▒   ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒▒▓  ▒ ░▒▓███▀▒ ██▒▒▒ ░░ ▒░ ░";
+echo "  ░   ░   ░ ▒ ▒░   ░ ▒ ▒░  ░ ▒  ▒ ▒░▒   ░▓██ ░▒░  ░ ░  ░";
+echo "░ ░   ░ ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░  ░  ░    ░▒ ▒ ░░     ░   ";
+echo "      ░     ░ ░      ░ ░     ░     ░     ░ ░        ░  ░";
+echo "                           ░            ░░ ░            ";
+         break;;
+        *)
+           echo -e "${RED}INCORRECT DAVID MURRAY WILL SHOOT U WITH HIS GLOCK NOWWWWWWW${ENDCOLOR}";;
+    esac
+done
+;;
+esac
